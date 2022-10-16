21a3de14ce98c248d90dfff50852e16cbcf28296
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-10-16 21:08:52 +0700

    hw done

---
+++ scripts421.sql
@@ -0,0 +1,7 @@
+ALTER TABLE student ADD CONSTRAINT age_constraint CHECK (age>15);
+
+ALTER TABLE student ADD CONSTRAINT name_unique UNIQUE(name);
+
+ALTER TABLE student ALTER age SET DEFAULT 20;
+
+ALTER TABLE faculty ADD CONSTRAINT name_color_unique UNIQUE (name, color);
---
+++ scripts423.sql
@@ -0,0 +1,9 @@
+SELECT student.name, student.age, f.name
+FROM student
+INNER JOIN faculty f
+ON student.faculty_id = f.id;
+
+SELECT student.name, student.age
+FROM student
+INNER JOIN avatar
+ON student.id = avatar.student_id;
