13fcdeb65e96a2223ef9450247279ce22be1ae88
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-10-19 23:43:42 +0700

    hw done

--- src/main/resources/liquibase/scripts/faculties.sql
+++ src/main/resources/liquibase/scripts/faculties.sql
@@ -3,3 +3,6 @@
 -- changeset tuzlukov:1
 CREATE INDEX faculties_name_index ON faculty(name);
 CREATE INDEX faculties_color_index ON faculty(color);
+
+-- changeset tuzlukov:2
+CREATE INDEX faculties_name_and_color_index ON faculty(name, color);
